90a043a9674d00a856fa0cdf6f958acea27d11b0
erfanmousavi141 <149951022+erfanmousavi141@users.noreply.github.com>
2023-11-24 21:58:01 +0330

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
 
-
-def merge(left_half, right_half) -> list:
+def merge(left_half: list, right_half: list) -> list:
     merged = []
     left_index = 0
     right_index = 0
-
     while left_index < len(left_half) and right_index < len(right_half):
         if left_half[left_index] <= right_half[right_index]:
             merged.append(left_half[left_index])
@@ -13,23 +11,21 @@ def merge(left_half, right_half) -> list:
         else:
             merged.append(right_half[right_index])
             right_index += 1
-
-    merged += left_half[left_index:]
-    merged += right_half[right_index:]
+    merged.extend(right_half[right_index:])
+    merged.extend(left_half[left_index:])
     return merged
 
 
-def merge_sort(lst: list) -> list:
-    lstLen = len(lst)
+def merge_sort(arr: list) -> list:
+    lstLen = len(arr)
     if lstLen <= 1:
-        return lst
+        return arr
     mid = lstLen // 2
-    lefts = lst[:mid]
-    rights = lst[mid:]
+    lefts = arr[:mid]
+    rights = arr[mid:]
     return merge(merge_sort(lefts), merge_sort(rights))
 
 
-array = [23, 43, 5, 4, 3, 2, 26, 8, 65, 43, 77, 93]
+array = [23, 43, 5, 4, 3, 2, 26, 8, 65, 93, 43]
 sorted_array = merge_sort(array)
-
 print(sorted_array)
